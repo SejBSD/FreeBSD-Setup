adf1031d9b3d9d5831d6750a8273b067fa79c746
Sejoslaw <k.dobrzynski94@gmail.com>
2023-01-15 15:09:38 +0100

    +games;

--- Setup_Games.sh
+++ Setup_Games.sh
@@ -5,16 +5,10 @@ sh ./Internal_Update.sh
 echo ""
 echo "################################################################"
 echo "##                                                            ##"
-echo "##                     Setting up Azure...                    ##"
+echo "##                     Setting up Games...                    ##"
 echo "##                                                            ##"
 echo "################################################################"
 echo ""
 
-read -p " ## Install VSCode with extensions? (yes/no) " _installVscode;
-
-if [ "$_installVscode" = "yes" ]
-then
-    sudo pkg install vscode
-
-    sh ./Internal_Common_VSCode_Azure.sh
-fi
+echo "## Installing minetest..."
+sudo pkg install minetest
