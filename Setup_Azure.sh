b0f9fd007ccdafe65826d3797159e8ad4b8520b9
Krzysztof Dobrzyński <k.dobrzynski94@gmail.com>
2023-01-15 15:01:53 +0100

    Container setup (#4)
    * +containers;
    
    * +containerd and podman options;
    
    * +minikube option;
    
    * +vm-bhyve option;

---
+++ Setup_Azure.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+sh ./Internal_Update.sh
+
+echo ""
+echo "################################################################"
+echo "##                                                            ##"
+echo "##                     Setting up Azure...                    ##"
+echo "##                                                            ##"
+echo "################################################################"
+echo ""
+
+read -p " ## Install VSCode with extensions? (yes/no) " _installVscode;
+
+if [ "$_installVscode" = "yes" ]
+then
+    sudo pkg install vscode
+
+    sh ./Internal_Common_VSCode_Azure.sh
+fi
---
+++ Setup_Containers.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+sh ./Internal_Update.sh
+
+echo ""
+echo "################################################################"
+echo "##                                                            ##"
+echo "##                  Setting up Containers...                  ##"
+echo "##                                                            ##"
+echo "################################################################"
+echo ""
+
+echo "## Installing bastille... ##"
+sudo pkg install bastille
+
+echo "## Installing buildah... ##"
+sudo pkg install buildah
+
+echo "## Installing kubectl... ##"
+sudo pkg install kubectl
+
+echo "## Installing helm... ##"
+sudo pkg install helm
+
+echo "## Installing containerd..."
+sudo pkg install containerd
+
+echo "## Installing podman..."
+sudo pkg install podman
+
+echo "## Installing minikube..."
+sudo pkg install minikube
+
+echo "## Installing vm-bhyve..."
+sudo pkg install vm-bhyve
+
+read -p "## Install VSCode? (yes/no) " _installVscode;
+
+if [ "$_installVscode" = "yes" ]
+then
+    sudo pkg install vscode
+
+    read -p "## Install extensions? (yes/no) " _installVscodeExtensions;
+
+    if [ "$_installVscodeExtensions" = "yes" ]
+    then
+        sh ./Internal_Common_VSCode_Extensions.sh
+
+        vscode --install-extension ms-kubernetes-tools.vscode-kubernetes-tools  # https://marketplace.visualstudio.com/items?itemName=ms-kubernetes-tools.vscode-kubernetes-tools
+    fi
+fi
